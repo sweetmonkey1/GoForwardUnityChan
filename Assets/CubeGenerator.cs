6314cdc787c59fc95242495fc146bf16b9d7df54
masaru sato <m.s.79741031@gmail.com>
2017-11-18 17:01:55 +0900

    Lesson7の課題提出
    ブロックに音声を加えました。

---
+++ Assets/CubeGenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeGenerator : MonoBehaviour {
+
+	public GameObject cubePrefab;
+
+	private float delta = 0;
+	private float span = 1.0f;
+	private float genPosx = 12;
+	private float offsetX = 0.5f;
+	private float spaceX = 0.4f;
+	private float offsetY = 0.3f;
+	private float spaceY = 6.9f;
+
+	private int maxBlockNum = 4;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.delta += Time.deltaTime;
+
+		if (this.delta > this.span) {
+			this.delta = 0;
+			int n = Random.Range (1, maxBlockNum + 1);
+			for (int i = 0; i < n; i++) {
+				GameObject go = Instantiate (cubePrefab) as GameObject;
+				go.transform.position = new Vector2 (this.genPosx, this.offsetY + i * spaceY);
+			}
+
+			this.span = this.offsetX + this.spaceX * n;
+		}
+	}
+}
---
+++ Assets/UnityChanController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityChanController : MonoBehaviour {
+
+	Animator animator;
+	Rigidbody2D rigid2D;
+
+	private float groundLevel = -3.0f;
+	private float dump = 0.8f;
+	private float deadLine = -9;
+
+	float jumpVelocity = 20;
+
+	// Use this for initialization
+	void Start () {
+		this.animator = GetComponent<Animator> ();
+		this.rigid2D = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.animator.SetFloat ("Horizontal", 1);
+
+		bool isGround = (transform.position.y > groundLevel) ? false : true;
+		this.animator.SetBool ("isGround", isGround);
+
+		GetComponent<AudioSource> ().volume = (isGround) ? 1 : 0;
+
+		if (Input.GetMouseButtonDown (0) && isGround) {
+			this.rigid2D.velocity = new Vector2 (0, this.jumpVelocity);
+		}
+
+		if (Input.GetMouseButton (0) == false) {
+			if (this.rigid2D.velocity.y > 0) {
+				this.rigid2D.velocity *= dump;
+			}
+		}
+
+		if (transform.position.x < this.deadLine) {
+			GameObject.Find ("Canvas").GetComponent<UIController>().GameOver ();
+
+			Destroy (gameObject);
+		}
+	}
+}
